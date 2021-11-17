06c3ac8205bdd63ae31d62592e7ddcf4f97a2820
Merrill Shen <50651325+merrillshen@users.noreply.github.com>
2021-11-17 17:36:17 +0000

    Ex 4 In-progress: Functions work but have not configured for gradual changing of power

--- dc_motor.c
+++ dc_motor.c
@@ -4,14 +4,18 @@
 // function initialise T2 and PWM for DC motor control
 void initDCmotorsPWM(int PWMperiod){
 	//initialise your TRIS and LAT registers for PWM
+    TRISEbits.TRISE2=0; //output on RE2
+    TRISCbits.TRISC7=0; //output on RC7
+    LATEbits.LATE2=0; // 0 output on RB0
+    LATCbits.LATC7=0; // 0 output on RB2
 
     // timer 2 config
-    T2CONbits.CKPS=???; // 1:??? prescaler
+    T2CONbits.CKPS=0b100; // 1:16 prescaler
     T2HLTbits.MODE=0b00000; // Free Running Mode, software gate only
     T2CLKCONbits.CS=0b0001; // Fosc/4
 
     // Tpwm*(Fosc/4)/prescaler - 1 = PTPER
-    T2PR=??; //Period reg 10kHz base period
+    T2PR=99; //Period reg 10kHz base period
     T2CONbits.ON=1;
     
     RE2PPS=0x0A; //PWM6 on RE2
@@ -51,24 +55,43 @@ void setMotorPWM(struct DC_motor *m)
 //function to stop the robot gradually 
 void stop(struct DC_motor *mL, struct DC_motor *mR)
 {
-
+    (*mL).power=0;
+    (*mR).power=0;
+    setMotorPWM(mL);
+    setMotorPWM(mR);
 }
 
 //function to make the robot turn left 
 void turnLeft(struct DC_motor *mL, struct DC_motor *mR)
 {
-
+    (*mL).direction=1;
+    (*mR).direction=1;
+    (*mL).power=0;
+    (*mR).power=75;
+    setMotorPWM(mL);
+    setMotorPWM(mR);
 }
 
 //function to make the robot turn right 
 void turnRight(struct DC_motor *mL, struct DC_motor *mR)
 {
- 
+    (*mL).direction=1;
+    (*mR).direction=1;
+    (*mL).power=75;
+    (*mR).power=0;
+    setMotorPWM(mL);
+    setMotorPWM(mR);
 }
 
 //function to make the robot go straight
 void fullSpeedAhead(struct DC_motor *mL, struct DC_motor *mR)
 {
-
+    (*mL).direction=1;
+    (*mR).direction=1;
+    // Increase/Decrease power to 50
+    (*mL).power=50;
+    (*mR).power=50;
+    setMotorPWM(mL);
+    setMotorPWM(mR);
 }
 
--- main.c
+++ main.c
@@ -9,26 +9,33 @@
 #include <xc.h>
 #include "rc_servo.h"
 #include "ADC.h"
+#include "dc_motor.h"
 
 #define _XTAL_FREQ 64000000 //note intrinsic _delay function is 62.5ns at 64,000,000Hz  
 
 void main(void){
-    LATDbits.LATD5=0;   //set initial output state
-    TRISDbits.TRISD5=0;
-    
     Interrupts_init();
     Timer0_init();
     ADC_init();     
-
-    unsigned int set_brightness = 60;
+    initDCmotorsPWM(99);
+    
+    struct DC_motor motorL, motorR; 		//declare two DC_motor structures 
+    unsigned int PWMcycle = 99;
+    motorL.power=0; 						//zero power to start
+    motorL.direction=1; 					//set default motor direction
+    motorL.dutyHighByte=(unsigned char *)(&PWM6DCH);	//store address of PWM duty high byte
+    motorL.dir_LAT=(unsigned char *)(&LATE); 		//store address of LAT
+    motorL.dir_pin=4; 						//pin RE4 controls direction
+    motorL.PWMperiod=PWMcycle; 			//store PWMperiod for motor
+    
+    motorR.power=0; 						//zero power to start
+    motorR.direction=1; 					//set default motor direction
+    motorR.dutyHighByte=(unsigned char *)(&PWM7DCH);	//store address of PWM duty high byte
+    motorR.dir_LAT=(unsigned char *)(&LATG); 		//store address of LAT
+    motorR.dir_pin=6; 						//pin RG6 controls direction
+    motorR.PWMperiod=PWMcycle; 			//store PWMperiod for motor
     
     while(1){
-		//write your code to call angle2PWM() to set the servo angle
-        if (ADC_getval() > set_brightness) {
-            angle2PWM(90);
-        }
-        else {
-            angle2PWM(-90);
-        }
+        turnRight(&motorL,&motorR);
     }
 }
